2dc8e70eac29d68e52c201349e19256d00b07710
vincent <vincent.bakpatina@ipnetinstitute.com>
2023-01-08 23:30:56 +0000

    initialize rest api

---
+++ udacity-c2-restapi/src/config/config.ts
@@ -0,0 +1,22 @@
+export const config = {
+  "dev": {
+    "username": "udagramvindev",
+    "password": "udagramvindev",
+    "database": "udagramvindev",
+    "host": "",
+    "dialect": "postgres",
+    "aws_region": "us-east-1",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagramvindev.ce64ioasdplq.us-east-1.rds.amazonaws.com"
+  },
+  "jwt": {
+    "secret": " "
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
